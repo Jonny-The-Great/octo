1035710c7f40a1caafbe4c6ac2a07224a6a3f6d3
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-17 09:13:32 +0200

    Update octo_netcommons.hpp

--- network 2/octo_netcommons.hpp
+++ network 2/octo_netcommons.hpp
@@ -1,1 +1,20 @@
+#ifndef OCTO_NETCOMMONS_HPP 
+#define OCTO_NETCOMMONS_HPP 
 
+#include <cstdint> 
+#include <array> 
+#include <span> 
+#include <algorithm> 
+
+namespace octo_network { 
+
+namespace octo_netcommons { 
+
+
+
+
+} // namespace octo_netcommons 
+
+} // namespace octo_network 
+
+#endif // OCTO_NETCOMMONS_HPP 
