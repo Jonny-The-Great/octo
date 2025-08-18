54b65a7475baa19d8cdc9836764c2bd9a2f43f87
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-18 18:23:43 +0200

    Update octo_ipv4.hpp

--- network 2/octo_ipv4.hpp
+++ network 2/octo_ipv4.hpp
@@ -8,16 +8,16 @@
  * @par Memory Layout (Offset and Size in Bytes) 
  * | Offset | Size | Field                      | 
  * |--------|------|----------------------------| 
- * | 0      | 1    | versionAndHeaderLength     | 
- * | 1      | 1    | typeOfService              | 
- * | 2      | 2    | totalLength                | 
- * | 4      | 2    | identification             | 
- * | 6      | 2    | flagsAndFragmentOffset     | 
- * | 8      | 1    | timeToLive                 | 
- * | 9      | 1    | protocol                   | 
- * | 10     | 2    | headerChecksum             | 
- * | 12     | 4    | sourceAddress              | 
- * | 16     | 4    | destinationAddress         | 
+ * | 0      | 1    | Version and header length  | 
+ * | 1      | 1    | Type of service            | 
+ * | 2      | 2    | Total length               | 
+ * | 4      | 2    | Identification             | 
+ * | 6      | 2    | Flags and fragment offset  | 
+ * | 8      | 1    | Time to live               | 
+ * | 9      | 1    | Protocol                   | 
+ * | 10     | 2    | Header checksum            | 
+ * | 12     | 4    | Source address             | 
+ * | 16     | 4    | Destination address        | 
  * Total size: 20 bytes (without options). 
  * 
  * @par Memory Layout (Offset and Size in Bits) 
@@ -244,6 +244,7 @@ void ChangeDestinationAddress(std::array<std::uint8_t>& ipv4HeaderByteArray, std
 } // namespace octo_network 
 
 #endif // OCTO_IPV4_HPP 
+
 
 
 
