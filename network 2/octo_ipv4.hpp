da0ac6f442c9c7016af2ccda5247e8a803420cc0
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-18 18:08:00 +0200

    Update octo_ipv4.hpp

--- network 2/octo_ipv4.hpp
+++ network 2/octo_ipv4.hpp
@@ -199,7 +199,7 @@ namespace utility
  * @brief Extracts the IPv4 flags and fields from the IPv4 header and stores it in the provided data buffer.  
  * 
  * This function extracts the version from the IPv4 header by using GetDataWithBinaryOffsetAndSize and places it into the  
- * provided data buffer in big endian order. The version is extracted from the first 4 bits of the `version_and_header_length` field. 
+ * provided data buffer.  
  * 
  * @param ipv4HeaderByteArray The IPv4 header as a raw byte array to extract the bits from. 
  * @param outputBuffer The output buffer to hold the extracted version, provided as a std::span. 
@@ -263,6 +263,7 @@ void ChangeDestinationAddress(std::array<std::uint8_t>& ipv4HeaderByteArray, std
 } // namespace octo_network 
 
 #endif // OCTO_HPP 
+
 
 
 
