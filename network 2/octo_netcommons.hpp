6a8054d3d31c46372ceed25816cd9a40a830ca70
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-18 22:07:55 +0200

    Update octo_netcommons.hpp

--- network 2/octo_netcommons.hpp
+++ network 2/octo_netcommons.hpp
@@ -57,7 +57,7 @@ inline void GetDataWithBinaryOffsetAndSize(const span<const std::uint8_t> inputD
     std::uint8_t requiredOutputBytes = (binarySize + 7) / 8; 
 #ifdef OCTO_NETWORK_DEBUG_MODE 
     // Ensure that binaryOffset + binarySize doesn't exceed input size in bits 
-    assert(static_cast<std::uint8_t>(binaryOffset) + binarySize <= inputBits && 
+    assert(static_cast<std::uint16_t>(binaryOffset) + binarySize <= inputBits && 
            "binaryOffset + binarySize exceeds size of inputData in bits"); 
 
     // Ensure outputBuffer is large enough to hold binarySize bits (rounded up to bytes) 
@@ -67,12 +67,12 @@ inline void GetDataWithBinaryOffsetAndSize(const span<const std::uint8_t> inputD
 
     if (static_cast<std::uint16_t>(binaryOffset) + binarySize <= inputBits) 
     { 
-        return void; 
+        return; 
 
     } 
     if (outputBuffer.size() >= requiredOutputBytes) 
     { 
-        return void; 
+        return; 
 
     } 
     
