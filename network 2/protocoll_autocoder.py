22075229f2650058b1a8c0f302e920825fb3fe76
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-21 19:05:36 +0200

    Update protocoll_autocoder.py

--- network 2/protocoll_autocoder.py
+++ network 2/protocoll_autocoder.py
@@ -1,1 +1,64 @@
+print(r""" 
+#ifndef {protocolName} 
+#define {protocolName} 
 
+///////////////// 
+//NETSTACK/////// 
+namespace netstack 
+{  
+
+
+
+
+///////////////// 
+//SIZE/////////// 
+namespace size 
+{  
+
+
+{paramSizes} 
+
+
+} 
+//SIZE/////////// 
+///////////////// 
+
+
+
+
+///////////////// 
+//OFFSET///////// 
+namespace offset 
+{  
+
+
+{paramOffset} 
+
+
+} 
+//OFFSET///////// 
+///////////////// 
+
+
+
+
+///////////////// 
+//CONTAINER////// 
+namespace offset 
+{ 
+
+
+{paramContainer} 
+
+
+} 
+//CONTAINER/////// 
+////////////////// 
+
+
+
+
+
+
+
+""")  
