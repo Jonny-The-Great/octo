ea6ef5d279b534458ff075b9a55a9b51d79cb3bd
Jonny-The-Great <Jonfh24@gmx.de>
2025-08-21 18:43:28 +0200

    Create protocoll_autocoder.pyScript.pyprotocoll_autocodeript.py

---
+++ network 2/protocoll_autocoder.pyScript.pyprotocoll_autocodeript.py
@@ -0,0 +1,18 @@
+
+Binflag1VariableName  = 
+Binflag2VariableName  = 
+Binflag3VariableName  = 
+Binflag4VariableName  = 
+Binflag5VariableName  = 
+Binflag6VariableName  = 
+Binflag7VariableName  = 
+Binflag8VariableName  = 
+Binflag9VariableName  = 
+Binflag10VariableName = 
+Binflag11VariableName = 
+Binflag12VariableName = 
+
+
+
+
+
